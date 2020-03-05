2392c3306e87ead08e4b98f4feeaa4410eb388af
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-03-05 22:12:19 +1100

    browser issue

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const cheerio = require('cheerio');
 
 const url =
   'https://www.airbnb.com.au/s/Japan/homes?query=Japan%20homes&place_id=ChIJLxl_1w9OZzQRRFJmfNR1QvU&refinement_paths%5B%5D=%2Fhomes&source=mc_search_bar&search_type=pagination&tab_id=home_tab&federated_search_session_id=4204db4e-e470-4b24-8634-b4f74d82cdaa&section_offset=4&items_offset=0';
-
-async function scrapeHolmesInIndexPage(url) {
+let browser;
+async function scrapeHomesInIndexPage(url) {
   try {
-    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto(url);
     const html = await page.evaluate(() => document.body.innerHTML);
@@ -29,8 +28,22 @@ async function scrapeHolmesInIndexPage(url) {
   }
 }
 
-async function main() {
-  const homes = await scrapeHolmesInIndexPage(url);
-  console.log(homes);
+async function scrapeDescriptionPage(Pageurl, page) {
+  try {
+    await page.goto(Pageurl);
+  } catch (err) {
+    console.log(err);
+  }
 }
+
+async function main() {
+  const browser = await puppeteer.launch({ headless: false });
+  // Instead of opening multiple pages for each urls - make a single page
+  const descriptionPage = await browser.newPage();
+  const homes = await scrapeHomesInIndexPage(url);
+  for (var i = 0; i < homes.length; i++) {
+    await scrapeDescriptionPage(homes[i], descriptionPage);
+  }
+}
+
 main();
